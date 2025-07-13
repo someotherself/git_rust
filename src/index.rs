5c6f0c0e5ea4a9bbe1418f377c31440d37280022
someotherself <romancristianv@gmail.com>
2025-07-13 17:22:55 +0300

    Index::index_file addded

--- src/index.rs
+++ src/index.rs
@@ -1,7 +1,11 @@
 #![allow(dead_code)]
-// use std::{fmt::{self, Display}, os::unix::fs::MetadataExt, path::PathBuf};
 
-use std::fmt::Display;
+use core::fmt;
+use std::{fmt::Display, io::Error, os::unix::fs::MetadataExt, path::PathBuf};
+
+use sha1::{Digest, Sha1};
+
+use crate::git_rust::{BASE_DIR, RepoRust};
 
 pub struct Index {
     header: IndexHeader,
@@ -38,7 +42,7 @@ pub struct IndexEntry {
     pub uid: u32,
     pub gid: u32,
     pub file_size: u32,
-    // pub object_id: [u8; 20],
+    pub object_id: [u8; 20],
     pub flags: u16,
 }
 
@@ -49,98 +53,124 @@ impl Display for Index {
     }
 }
 
-// #[allow(dead_code)]
-// impl Index {
-//     fn header(&self) -> &IndexHeader {
-//         &self.header
-//     }
+#[allow(dead_code)]
+impl Index {
+    fn header(&self) -> &IndexHeader {
+        &self.header
+    }
 
-//     fn build_index(path: String) -> std::io::Result<()> {
-//         // get root
-//         // Check if index already exists
-//         let mut entries: Vec<IndexEntry> = Vec::new();
-//         let path = PathBuf::from(path);
-//         if path.is_file() {
-//             // Create entry
-//             // Add to vec
-//             // Return the index
-//         } else if path.is_dir() {
-//             entries = Index::index_dir(path)?;
-//         }
-//         todo!()
-//     }
+    fn build_index(path: String) -> std::io::Result<Index> {
+        let root = &RepoRust::get_root()?.base_path;
+        if root.join(BASE_DIR).join("INDEX").exists() {
+            return Err(Error::other("INDEX already exists"));
+        }
+        let mut entries: Vec<IndexEntry> = Vec::new();
+        let path = PathBuf::from(path);
+        if path.is_file() {
+            entries.push(Index::index_file(path)?);
+        } else if path.is_dir() {
+            // entries = Index::index_dir(path)?;
+        }
+        // Build the index and return
+        todo!()
+    }
 
-//     fn index_dir(path: PathBuf) -> std::io::Result<Vec<IndexEntry>> {
-//         let mut entries: Vec<IndexEntry> = Vec::new();
-//         for node in path.read_dir()? {
-//             let node = node?;
-//             match node.file_type()? {
-//                 n if n.is_dir() => {
-//                     // recurse???
-//                 }
-//                 n if n.is_file() => {
-//                     // Write blob
-//                     // return SHA-1
-//                 }
-//                 _ => continue,
-//             }
-//         }
-//         todo!()
-//     }
+    fn sha1_entry(file: Vec<u8>) -> std::io::Result<[u8; 20]> {
+        let mut hasher = Sha1::new();
+        hasher.update(&file);
+        let result = hasher.finalize();
+        Ok(result.into())
+    }
 
-// #[allow(unused_variables)]
-//     pub fn index_file(path: PathBuf) -> std::io::Result<IndexEntry> {
-//         let metadata = path.metadata()?;
-//         let ctime = metadata.ctime() as u32;
-//         let ctime_nanos = metadata.ctime_nsec() as u32;
-//         let mtime = metadata.mtime() as u32;
-//         let mtime_nanos = metadata.mtime_nsec() as u32;
-//         let dev = metadata.dev() as u32;
-//         let ino = metadata.ino() as u32;
-//         let mode = metadata.mode() as u32;
-//         let uid = metadata.uid();
-//         let gid = metadata.gid();
-//         let file_size = metadata.size() as u32;
+    fn index_dir(path: PathBuf) -> std::io::Result<Vec<IndexEntry>> {
+        let mut _entries: Vec<IndexEntry> = Vec::new();
+        for node in path.read_dir()? {
+            let node = node?;
+            match node.file_type()? {
+                n if n.is_dir() => {
+                    // recurse???
+                }
+                n if n.is_file() => {
+                    // write entry
+                    // return SHA-1
+                }
+                _ => continue,
+            }
+        }
+        todo!()
+    }
 
-//         let object_id: [u8; 20];
+    #[allow(unused_variables)]
+    pub fn index_file(path: PathBuf) -> std::io::Result<IndexEntry> {
+        let metadata = path.metadata()?;
+        let ctime = metadata.ctime() as u32;
+        let ctime_nanos = metadata.ctime_nsec() as u32;
+        let mtime = metadata.mtime() as u32;
+        let mtime_nanos = metadata.mtime_nsec() as u32;
+        let dev = metadata.dev() as u32;
+        let ino = metadata.ino() as u32;
+        let mode = metadata.mode();
+        let uid = metadata.uid();
+        let gid = metadata.gid();
+        let file_size = metadata.size() as u32;
 
-//         let path_str = path.to_string_lossy();
-//         let name_len = path_str.len();
-//         let assume_valid = false;
-//         let extended = false;
-//         let stage = 0b00;
-//         let name_len_field = if name_len >= 0xFFF { 0xFFF } else { name_len as u16 };
-//         let flags: u16 = ((assume_valid as u16) << 15)
-//             | ((extended as u16) << 14)
-//             | ((stage as u16) << 12)
-//             | name_len_field;
+        let file = std::fs::read(PathBuf::from(&path))?;
+        let object_id = Index::sha1_entry(file)?;
 
-//         let path_bytes = path_str.as_bytes();
-//         let padding = (8 - ((62 + path_bytes.len()) % 8)) % 8;
+        let path_str = path.to_string_lossy();
+        let name_len = path_str.len();
+        let assume_valid = false;
+        let extended = false;
+        let stage = 0b00;
+        let name_len_field = if name_len >= 0xFFF {
+            0xFFF
+        } else {
+            name_len as u16
+        };
+        let flags: u16 = ((assume_valid as u16) << 15)
+            | ((extended as u16) << 14)
+            | ((stage as u16) << 12)
+            | name_len_field;
 
-//         let entry = IndexEntry {ctime, ctime_nanos, mtime, mtime_nanos, dev, ino, mode, uid, gid, file_size, object_id, flags};
-//         Ok(entry)
-//     }
+        let path_bytes = path_str.as_bytes();
+        let padding = (8 - ((62 + path_bytes.len()) % 8)) % 8;
 
-//     fn update_index() {}
-// }
+        let entry = IndexEntry {
+            ctime,
+            ctime_nanos,
+            mtime,
+            mtime_nanos,
+            dev,
+            ino,
+            mode,
+            uid,
+            gid,
+            file_size,
+            object_id,
+            flags,
+        };
+        Ok(entry)
+    }
 
-// impl fmt::Display for IndexEntry {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         write!(
-//             f,
-//             "ctime: {}.{}  mtime: {}.{}  dev: {}  ino: {}  mode: {:o}  uid: {}  gid: {}  size: {}  flags: {}",
-//             self.ctime,
-//             self.ctime_nanos,
-//             self.mtime,
-//             self.mtime_nanos,
-//             self.dev,
-//             self.ino,
-//             self.mode,
-//             self.uid,
-//             self.gid,
-//             self.file_size,
-//             self.flags,
-//         )
-//     }
-// }
+    fn update_index() {}
+}
+
+impl fmt::Display for IndexEntry {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "ctime: {}.{}  mtime: {}.{}  dev: {}  ino: {}  mode: {:o}  uid: {}  gid: {}  size: {}  flags: {}",
+            self.ctime,
+            self.ctime_nanos,
+            self.mtime,
+            self.mtime_nanos,
+            self.dev,
+            self.ino,
+            self.mode,
+            self.uid,
+            self.gid,
+            self.file_size,
+            self.flags,
+        )
+    }
+}
